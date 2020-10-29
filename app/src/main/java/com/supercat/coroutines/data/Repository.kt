a2e79146145726ec4f3fb8ff4e1228de0f077fc8
Nikita <timchenkonikita123@gmail.com>
2020-10-29 17:35:25 +0300

    fix: Removed useless code

--- app/src/main/java/com/supercat/coroutines/data/Repository.kt
+++ app/src/main/java/com/supercat/coroutines/data/Repository.kt
@@ -1,12 +1,8 @@
 package com.supercat.coroutines.data
 
-import android.util.Log
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.withContext
 import kotlin.random.Random
 
 enum class Status {
@@ -22,10 +18,10 @@ class Repository(private val dao: EntityDAO) {
     suspend fun addElement() {
         state.value = Status.DATA_INSERTING
         delay(3000)
-        Log.i("VVV2", Thread.currentThread().name)
-        withContext(Dispatchers.IO) {
-            dao.addElement(Entity(randomId = Random.nextInt()))
-        }
+
+        dao.addElement(Entity(randomId = Random.nextInt()))
+
+
         state.value = Status.IDLE
     }
 
